988a690612dec738892fa89cd6d25360c6b8ec59
Tung Tran Thanh <tungtt22@fsoft.com.vn>
2019-11-25 16:57:55 +0700

    update to fix issue

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,9 @@ Main lambda function
 """
 
 import datetime
+import dateutil
 import boto3
+from dateutil.relativedelta import relativedelta
 
 TODAY = datetime.date.today()
 TODAY_STRING = TODAY.strftime('%Y/%m/%d')
@@ -33,7 +35,6 @@ class Snapshot():
     """
     handle all action with snapshot
     """
-
     def __init__(self, ec2):
         """
         Init method
@@ -47,23 +48,26 @@ class Snapshot():
         """
         # We only want to look through instances with
         # the following tag key value pair: auto_snapshot : true
-        instances = ec2.instances.filter(Filters=[{
-            'Name': 'tag:auto_backup',
-            'Values': ['true']
-        }])
+        instances = ec2.instances.filter(
+            Filters=[{
+                'Name': 'instance-state-name',
+                'Values': ['running', 'stopped']
+            }])
         return instances
 
     @staticmethod
-    def find_tag(instance, tag_value):
+    def find_tag(instance, tag_key):
         """
         find the instance tag name
         """
         for tag in instance.tags:  # Get the name of the instance
-            if tag['Key'] == tag_value:
+            if tag['Key'] == tag_key:
                 tag_name_value = tag['Value']
-        print('[+] Found tagged instance \'{1}\', id: {0}, state: {2}'.format(
-            instance.id, tag_name_value, instance.state['Name']))
-        return tag_name_value
+                # print('[+] Found tagged instance \'{1}\', id: {0}, state: {2}'.format(
+                #     instance.id, tag_name_value, instance.state['Name']))
+                return tag_name_value
+
+        return None
 
     @staticmethod
     def create_snapshot(vol, tag_name_value):
@@ -79,24 +83,21 @@ class Snapshot():
             format(tag_name_value, vol.id, TODAY_STRING), )
         snapshot.create_tags(  # Add the following tags to the new snapshot
             Tags=[{
-                'Key': 'auto_backup',
+                'Key': 'AutoBackup',
                 'Value': 'true'
             }, {
-                'Key': 'volume',
+                'Key': 'Volume',
                 'Value': vol.id
             }, {
                 'Key': 'CreatedOn',
                 'Value': TODAY_STRING
             }, {
-                'Key':
-                'Name',
-                'Value':
-                '{}-autobackup'.format(tag_name_value) + '-' + TODAY_STRING
+                'Key': 'Name',
+                'Value': '{0}-AutoBackup-{1}'.format(tag_name_value, TODAY_STRING)
             }])
-
-        print("[+] Snapshot completed")
+        print('[+] Snapshot completed')
         COUNTER["snapshot_created"] += 1
-        COUNTER["snapshot_created_size"] = snapshot.volume_size
+        COUNTER["snapshot_created_size"] += snapshot.volume_size
 
     @staticmethod
     def delete_snapshot(snapshot):
@@ -108,24 +109,26 @@ class Snapshot():
             # cleated on date, name and auto_snap tag
             if tag['Key'] == 'CreatedOn':
                 created_on_string = tag['Value']
-            if tag['Key'] == 'auto_backup' and tag['Value'] == 'true':
+            if tag['Key'] == 'AutoBackup' and tag['Value'] == 'true':
                 can_delete = True
             if tag['Key'] == 'Name':
                 name = tag['Value']
-        created_on = datetime.datetime.strptime(
-            created_on_string, '%Y/%m/%d').date()
+        created_on = datetime.datetime.strptime(created_on_string,
+                                                '%Y/%m/%d').date()
 
         if created_on <= DELETION_DATE and can_delete:
+            snapshot_size = 0
             print(
                 '[+] Snapshot id {0}, ({1}) from {2} is {3} or more days old... deleting'
                 .format(snapshot.id, name, created_on_string,
                         DELETE_AFTER_DAY))
+            snapshot_size = snapshot.volume_size
             response = snapshot.delete()
             if response:
-                COUNTER["snapshot_deleted"] += snapshot.volume_size
-                COUNTER["snapshot_deleted_size"] += 1
+                COUNTER["snapshot_deleted"] += 1
+                COUNTER["snapshot_deleted_size"] += snapshot_size
 
-    def create_all_snapshot(self):
+    def create(self):
         """
         Create snapshot for instance
         """
@@ -134,17 +137,21 @@ class Snapshot():
         instances = Snapshot.get_instances(ec2)
 
         for _inst in instances.all():
+            auto_backup_tag = Snapshot.find_tag(_inst, 'auto_backup')
             tag_name_value = Snapshot.find_tag(_inst, 'Name')
 
-            # Iterate through each instance's volumes
-            vols = _inst.volumes.all()
-            for vol in vols:
-                Snapshot.create_snapshot(vol, tag_name_value)
+            if auto_backup_tag == "true":
+                print(
+                    '[+] Found tagged instance allow auto backup \'{1}\', id: {0}'
+                    .format(_inst.id, tag_name_value))
+                # Iterate through each instance's volumes
+                vols = _inst.volumes.all()
+                for vol in vols:
+                    Snapshot.create_snapshot(vol, tag_name_value)
 
-
-    def delete_all_snapshot(self):
+    def delete(self):
         """
-        Delete all snapshot out of date
+        Create snapshot for instance
         """
         ec2 = self.ec2
         instances = Snapshot.get_instances(ec2)
@@ -174,16 +181,16 @@ def lambda_handler(event, context):
         print('[+] Instances in EC2 Region {0}:'.format(_reg))
         ec2 = boto3.resource('ec2', region_name=_reg)
         snapshot = Snapshot(ec2)
-        snapshot.create_all_snapshot()
-        snapshot.delete_all_snapshot()
+        snapshot.create()
+        snapshot.delete()
 
     print('[+] Result:')
     print(
-        ' Total snapshot created: {0}\n Total size of snapshots created: {1} GB'
+        ' Total item snapshot created: {0}\n Total size of snapshots created: {1} GB'
         .format(COUNTER["snapshot_created"], COUNTER["snapshot_created_size"]))
 
     print(
-        ' Total snapshot deleted: {0}\n Total size of snapshots deleted {1} GB'
+        ' Total item snapshot deleted: {0}\n Total size of snapshots deleted {1} GB'
         .format(COUNTER["snapshot_deleted"], COUNTER["snapshot_deleted_size"]))
 
     return
